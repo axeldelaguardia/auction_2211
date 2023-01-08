ad4a0cdf48df293912f0ea140b52f1e8fef21d18
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-08 14:46:56 -0700

    Add: #initialize test for Auction

---
+++ spec/auction_spec.rb
@@ -0,0 +1,19 @@
+require './spec/spec_helper'
+
+describe Auction do
+	let(:item1) {Item.new('Chalkware Piggy Bank')}
+	let(:item2) {Item.new('Bamboo Picture Frame')}
+	let(:attendee) {Attendee.new(name: 'Megan', budget: '$50')}
+	let(:auction) {Auction.new}
+
+	describe '#initialize' do
+		it 'exists' do
+			expect(auction).to be_a(Auction)
+		end
+
+		it 'has attributes' do
+			expect(auction.items).to eq([])
+		end
+	end
+
+end
