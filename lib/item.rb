630b8104cc9ca05a6731a8ccb1bfedc9ae1968e9
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-08 16:48:08 -0700

    Add: #close_bidding and update #add_bid

--- lib/item.rb
+++ lib/item.rb
@@ -5,13 +5,22 @@ class Item
 	def initialize(name)
 		@name = name
 		@bids = {}
+		@bidding_open = true
 	end
 
 	def add_bid(attendee, bid)
-		bids[attendee] = bid
+		if @bidding_open
+			bids[attendee] = bid
+		else
+			'Bidding Closed'
+		end
 	end
 
 	def current_high_bid
 		bids.values.max
+	end
+
+	def close_bidding
+		@bidding_open = false
 	end
 end
